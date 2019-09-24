549e26b1ec3b9e2d2b8e6784a0b7534c042a3112
PeterKimG <myopinion@naver.com>
2019-09-24 10:40:36 +0900

    web3, bcryptjs updated

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-var bcrypt = require("bcrypt-nodejs");
+var bcrypt = require("bcryptjs");
 
 // schema // 1
 var userSchema = mongoose.Schema({
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var Post = require("../models/Post");
+var Post = require("../models/post");
 var util = require("../util")
 
 // Index 
