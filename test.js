a508f833c6f6e91701c0c418808e14000919f791
PeterKimG <myopinion@naver.com>
2019-11-07 15:44:07 +0900

    git setting

--- test.js
+++ test.js
@@ -1,63 +1,10 @@
-var express = require('express');
-var router = express.Router();
-var util = require("./util");
-var User = require("./models/user");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
-var flash = require("connect-flash");
-var session = require("express-session");
-var passport = require("./config/passport");
-var mongo_db = "mongodb+srv://gkskanj:djfudnsqlalfqjsgh@cluster0-vb9pr.mongodb.net/test?retryWrites=true&w=majority"
-var app = express();
+const bufferText = Buffer.from('hello world', 'utf8');
+console.log(bufferText);
 
 
-// DB setting
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.connect(mongo_db);
-var db = mongoose.connection;
-db.once("open", function(){
-  console.log("DB connected");
-});
-db.on("error", function(err){
-  console.log("DB ERROR : ", err);
-});
+const text = bufferText.toString('hex');
+console.log(text);
 
-// Other settings
-app.set("view engine", "ejs");
-app.use(express.static(__dirname+"/public"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(methodOverride("_method"));
-app.use(flash());
+console.log(bufferText.toString());
 
-//session password
-app.use(session({secret:"myopinion", resave: true, saveUninitialized: true}))
-
-//passport setting
-app.use(passport.initialize());
-app.use(passport.session());
-
-//custom middlewares - req.local contains below two things, params can be used to ejs
-app.use(function(req, res, next){ 
-  res.locals.isAuthenticated = req.isAuthenticated(); // login check; ture and false
-  res.locals.currentUser = req.user; // pull user information
-  next();
-})
-
-// Routes
-
-app.get('/', function(req, res) {
-    res.send(`${User.findOne().select('username')}`);
-    console.log(`${User.findeOne(username)}`)
-  });
-
-// Port setting
-var port = 3000
-app.listen(port, function(){
-  console.log("server on! http://localhost:"+port); //
-});
-
+console.log('0x'+Buffer.from('hello world').toString('hex'));
