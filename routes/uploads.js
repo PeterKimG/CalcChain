d3bd28d1109197eb16010a49472d19e53369df45
PeterKimG <myopinion@naver.com>
2019-11-12 15:56:55 +0900

    filehash value can be send Tx with data value(hex)

--- routes/uploads.js
+++ routes/uploads.js
@@ -6,15 +6,24 @@ var User = require("../models/user");
 var fs = require ('fs');
 var crypto = require('crypto');
 var hash = crypto.createHash('md5');
+const web3 = require('web3');
 var Fileinfo = require('../models/fileinfo');
+var Wallet = require("../models/wallet")
 // var fd = fs.createReadStream('/ path and file name.name');
+
+//Infura HttpProvider Endpoint
+var Web3 = new web3(new web3.providers.HttpProvider("https://ropsten.infura.io/v3/66f5bc220371494cb3465fca20893eb4"));
 
 router.get("/:username", util.isLoggedin, function(req, res){
     User.findOne({username:req.params.username}, function(err, user){
         if(err) return res.json(err);
         fs.readdir(`uploads/${req.user.username}`, 'utf8' , function (error, filelist) {
             var length = filelist.length
-            res.render("files/uploads", {user:user, filelist, length});
+            Wallet.findOne(req.body)
+                .populate("owner")
+                .exec(function (err, wallet) {
+                    res.render("files/uploads", {user:user, filelist, length, wallet:wallet});
+            });
         })
     });
 });
@@ -31,43 +40,70 @@ var _storage = multer.diskStorage({
 var upload = multer({storage:_storage})
 
 router.post('/:username', upload.single('userfile'), function(req, res){
+    // req.body.owner = req.user._id;
     User.findOne({username:req.params.username}, function(err, user){
         if(err) return res.json(err);
-        console.log(req.file);
-        let filename = req.file.filename;
-        let originalname = req.file.originalname;
-        let filetype = req.file.mimetype;
-        let input = fs.createReadStream(`${req.file.path}`);
-        input.on('readable', function() {
-            var data = input.read();
-            if (data) {
-                hash.update(data);
-            } else {
-                let fileHash = hash.digest('hex')
-                // console.log(`${hash.digest('hex')}`);
-                let fileinfo = new Fileinfo({
-                    uploader: user._id,
-                    uploadername: user.username,
-                    originalname: req.file.originalname,
-                    filename: req.file.filename,
-                    filehash: fileHash,
-                    // Txhash: '',
-                    // fileTx: false
-                });
-                fileinfo.save(function (err, fileinfo) {
-                    if (err){
-                        req.flash("fileinfo", req.body);
-                        req.flash("errors", util.parseError(err));
-                        return console.error(err);
-                    }
-                    res.render("files/redirect", {
-                        user,
-                        filename,
-                        originalname,
-                        filetype    
+        Wallet.findOne(req.body)
+            .populate("owner")
+            .exec(async function (err, wallet) {
+                if (err) return res.json(err);
+                console.log(wallet);
+                let filename = req.file.filename;
+                let originalname = req.file.originalname;
+                let filetype = req.file.mimetype;
+                const myAccount = "0xdee5F53B29FDB3996fb546026fDdf49adc6D4a89"
+                let input = fs.createReadStream(`${req.file.path}`);
+                input.on('readable', async function() {
+                    var data = input.read();
+                    if (data) {
+                        hash.update(data);
+                    } else {
+                        let fileHash = hash.digest('hex')
+                        // console.log(`${hash.digest('hex')}`);
+                        let sendTx = await Web3.eth.accounts.signTransaction({
+                            to: myAccount,
+                            value: 100000,
+                            gas: 210000,
+                            data: '0x'+ fileHash
+                        }, wallet.privateKey, async function (err, result) {
+                            if (err) {
+                                console.error(err);
+                                return;
+                            }
+                            console.log(`rawTransaction ${result.rawTransaction}`);
+                            await Web3.eth.sendSignedTransaction(result.rawTransaction, function (err, result3) {
+                                if (err) {
+                                    console.error(err);
+                                    return;
+                                } else {
+                                    console.log(result3);
+                                    let fileinfo = new Fileinfo({
+                                        uploader: user._id,
+                                        uploadername: user.username,
+                                        originalname: req.file.originalname,
+                                        filename: req.file.filename,
+                                        filehash: fileHash,
+                                        Txhash: result3,
+                                        // fileTx: false
+                                    });
+                                    fileinfo.save(function (err, fileinfo) {
+                                        if (err){
+                                            req.flash("fileinfo", req.body);
+                                            req.flash("errors", util.parseError(err));
+                                            return console.error(err);
+                                        }
+                                        res.render("files/redirect", {
+                                            user,
+                                            filename,
+                                            originalname,
+                                            filetype   
+                                        })
+                                    })
+                                }
+                        })
                     })
-                })
-            }
+                }
+            })
         })
     })
 })
